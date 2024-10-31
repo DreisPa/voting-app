2802abae0eac1984ce3d628345b64e39d8b308d2
DreisPa <132481982+DreisPa@users.noreply.github.com>
2024-11-01 03:32:58 +0500

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,1 +1,15 @@
+const socket = io();
+
+document.getElementById('yesButton').addEventListener('click', () => {
+    socket.emit('vote', 'yes');
+});
+
+document.getElementById('noButton').addEventListener('click', () => {
+    socket.emit('vote', 'no');
+});
+
+socket.on('updateVotes', (votes) => {
+    document.getElementById('yesVotes').innerText = votes.yes;
+    document.getElementById('noVotes').innerText = votes.no;
+});
 
